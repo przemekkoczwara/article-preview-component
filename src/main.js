fd2e28d6f0f74f4241b227bb3c569a1f0b378b26
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-11-12 20:29:58 +0100

    Add RWD for footer, tablet & destktop footer is toltip

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import './sass/main.scss';
 
+const button = document.getElementById('button');
+const detailsAbout = document.querySelector('.details__about');
+const footer = document.getElementById('footer');
+
+button.addEventListener('click', function () {
+  const viewportWidth = window.innerWidth;
+  if (viewportWidth < 768) {
+    detailsAbout.classList.toggle('hide');
+    footer.classList.toggle('active');
+  } else {
+    footer.classList.toggle('active');
+  }
+});
